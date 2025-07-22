971429332b17be40eb6eb2cb8f15af0da0e30530
Florian Bury <bury@hectronic.com>
2025-07-22 16:31:12 +0200

    ADD Fix index

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace placeholders with environment variables
-envsubst '${COOKIEBOT_ID} ${GA_ID}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+envsubst '${COOKIEBOT_ID} ${GA_ID}' < /usr/share/nginx/html/index.html.template > /usr/share/nginx/html/index.html
 
 # Start nginx
 exec "$@"
